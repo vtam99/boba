9fc8bc6efd237fad02e4043a4a408ed39a0ad83d
vtam99 <46866876+vtam99@users.noreply.github.com>
2019-01-20 09:27:52 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include "inc/html-top.inc"; ?>
+
+<title>Home | Midterm Project</title>
+
+<?php include "inc/html-top2.inc"; ?>
+
+<body>
+
+    <header>
+            <h1>Best Boba Tea Shops</h1>  
+    </header>
+
+    <section class="fixed">
+        <h2 class="newsletter">Sign up for our boba tea newsletter</h2>
+        <form>
+            <input type="text" placeholder="Enter your email here" class="signup">
+            <input type="submit" value="Sign up!" class="click">
+        </form>
+    </section>
+
+    <main>
+        <img src="images/bobatea.jpg" class="bobateapic" alt="Picture of boba tea">
+
+    </main> 
+
+    <div class="container">
+        <section class="weak">
+            <h2>Today's trendy drink</h2>
+        </section>
+
+        <section class="terminal">
+            <h2>
+                <a href="stores.php" class="readon">Click here to learn more!</a>
+            </h2>
+        </section>
+    </div>
+    
+</body>
+</html>
---
+++ stores.php
@@ -0,0 +1,94 @@
+<?php include "inc/html-top.inc"; ?>
+
+<title>Best NYC Boba Tea Shops | Midterm Project</title>
+
+<?php include "inc/html-top2.inc"; ?>
+
+<body>
+
+    <header>
+        <h1><a href="index.php">Best Boba Tea Shops</a></h1>       
+    </header>
+
+    <section class="fixed">
+        <h2 class="newsletter">Sign up for our boba tea newsletter</h2>
+        <form>
+            <input type="text" placeholder="Enter your email here" class="signup">
+            <input type="submit" value="Sign up!" class="click">
+        </form>
+    </section>
+
+        <main>
+            <section class="intro">
+                <p>Boba tea has been stirred up all over New York City for the past year and can be found at an estabilisahed chain or locally owned shop. The drink was originallly invented in the 1980s in tea shops in Taichung, Taiwan. As in the name, most boba tea drinks contain a tea base mixed with fruit flavor or milk. The fun of this drink comes from the small chewy tapioca balls, also known as boba, which is why it is sometimes called “the drink with the fat straw”. Below are some of the best boba tea shops in New York City, according to Time Out's list of best places to get boba tea in NYC.</p>
+
+                <div class="line">
+                    <img src="images/line.jpg" alt="Picture of line under paragraph">
+                </div>
+            </section>
+
+            <section class="content">
+
+                <h2 class="storename">Boba Guys</h2>
+
+                <section>
+                <h3>About</h3>
+                <p class="bobaguysinfo">Boba Guys serves the highest quality boba milk tea in the world. They specialize in tea drinks and make their own tapioca pearls for boba tea. The owners also swapped processed powders and artificial sweeteners for house-made cane-sugar syrups and organic milk.</p>
+
+                <h3>Popular Drinks</h3>
+                <p>The most popular drinks from Boba Guys include Classic Black Tea and Lychee Green Tea. In the specialty section of the menu, customers can choose from Hong Kong Style Milk Tea and the Strawberry Matcha Latte. Some drinks are also customizable, which means customers can choose the kind of milk in their tea and the sweetness level.</p>
+               
+
+                </section>
+
+                <figure class="bobaguys">
+                    <img src="images/bobaguystea.jpg" alt="Picture of the Classic Milk Tea">
+                    <figcaption>Boba Guy's Classic Milk Tea with boba</figcaption>
+                </figure>
+                
+           
+                <h2 class="storename">Gong Cha</h2>
+
+                <section>
+                <h3>About</h3>
+                <p>Gong Cha was founded in Kaohsiung, Taiwan in 2006. The name Gong Cha translates to "tribute tea for the emperor." All of the teas are freshly brewed every four hours and each drink is customized with the customer's ice and sugar level. The tea used is sourced from the finest Taiwan tea esates. </p>
+
+                <h3>Popular Drinks</h3>
+                <p>Gong Cha's house brand specialty is their "Milk Foam" tea, which is described as a "slightly salted cream that sits on top of freshly brewed tea." Other popular drinks include Pearl Milk Tea, Lychee Oolong, and Brown Sugar Milk Tea.</p>
+
+                </section>
+
+                <figure class="gongcha">
+                    <img src="images/gongchatea.jpg" alt="Picture of the Oolong Milk Tea">
+                    <figcaption>Gong Cha's Oolong Milk Foam Tea with boba</figcaption>
+                </figure>
+               
+
+                <h2 class="storename">Bar Pa Tea</h2>
+
+                <section>
+                <h3>About</h3>
+                <p>Bar Pa Tea emphasizes flavor over sweetness, creating a more healthful alternative to the original beverage without compromising the distinct taste and experience of traditional boba tea. The recipes feature boba that is free of additives and drinks sweetened with pure cane sugar instead of corn syrup.</p>
+
+                <h3>Popular Drinks</h3>
+                <p>Bar Pa Tea's most popular drinks are their tea lattes, such as Oolong Matcha tea, Brown Sugar Oolong, and Iron Goddess which can be made hot or cold. Other popular drinks include matcha lemonade and fresh grapefruit yakult. Besdies the regular sized boba, Bar Pa Tea also has mini sized ones as their topping.</p>
+
+                </section>
+
+                <figure class="barpatea">
+                    <img src="images/barpatea.jpg" alt="Picture of the Matcha Tea Latte">
+                    <figcaption>Bar Pa Tea's Matcha Tea Latte with boba </figcaption>
+                </figure>
+                
+
+            </section>
+       
+        </main>
+
+    <footer>
+        <div>This webpage, by Valerie Tam</div>
+    </footer>
+
+</body>
+
+</html>
